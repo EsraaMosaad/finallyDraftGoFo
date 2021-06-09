a9f990f56b6097c5fb754cff1faedcd4ff70c9da
EsraaMosaad <70305108+EsraaMosaad@users.noreply.github.com>
2021-06-09 09:14:32 +0200

    Add files via upload

---
+++ code/H4-GoFo/Timeslote.h
@@ -0,0 +1,16 @@
+#ifndef TIMESLOTE_H
+#define TIMESLOTE_H
+
+
+class Timeslote
+{
+    public:
+        Timeslote() {}
+        virtual ~Timeslote() {}
+
+    protected:
+
+    private:
+};
+
+#endif // TIMESLOTE_H
